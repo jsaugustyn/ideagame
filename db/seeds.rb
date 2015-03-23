9793563af9a92aa5c684a0f909c7091fe9d2636a
jsaugustyn <js.augustyn@gmail.com>
2015-03-23 10:34:19 -0400

    Change seeds to include actual trends

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Trend.create(title: "Mobile", description: "Create an app that works with mobile devices.")
-Trend.create(title: "Web App", description: "Create an app that works on the internet.")
-Trend.create(title: "Wearables", description: "Create an app that works with Apple watches or fitbits.")
-Trend.create(title: "Email", description: "Make it work with email.")
-Trend.create(title: "Audio", description: "Create an interesting UI that is audio based.")
-Trend.create(title: "Video", description: "Create something interesting based on videos.")
-Trend.create(title: "Streaming", description: "Make something live.")
+Trend.create(title: "Autonomous systems", description: "Robots and such.")
+Trend.create(title: "3D & 4D printing", description: "3D printing “prints” physical objects layer-by-layer in an additive fashion, unlike molding, machining, or other traditional techniques.")
+Trend.create(title: "Analytics", description: "We are awash in data, but only just beginning to develop the tools to mine and visualize it to support complex decisions. While “Big Data” has become a buzzword, the real revolution lies in analytics.")
+Trend.create(title: "Human augmentation", description: "Over the next 30 years technologies will emerge that allow us to transcend limits on human potential.")
+Trend.create(title: "Mobile & cloud computing", description: "Mobile and cloud computing are transforming the way people interact with data. ")
+Trend.create(title: "Future of medicine", description: "Over the next 30 years medicine will be completely transformed. ")
+Trend.create(title: "Cyber", description: "Over the next 30 years the rise of the Internet of Things and growing interdependence among connected aspects of everyday life will bring cybersecurity to the forefront")
+Trend.create(title: "Energy", description: "While global dependence on oil is set to decline, new frictions will emerge over access to rare materials used in batteries, solar cells, and other linchpins of the energy revolution.")
+Trend.create(title: "Changing nature of work", description: "Technology is underwriting a significant change in the relationship between individuals and their work. ")
+Trend.create(title: "Sustainable cities", description: "By 2045, close to 6.4 billion people will live in cities, most located in developing nations. With well over half of the global population living in cities, urban centers will exert an enormous influence on how the future evolves.")
+Trend.create(title: "Internet of things", description: "Over the next 30 years embedded sensors, actuators, and data communications technology will rewrite how we think about information networks.")
+Trend.create(title: "Food and water technology", description: "Over the next 30 years, access to food and water will become a crucial driver of global tension and technological innovation. ")
+Trend.create(title: "Quantum computing", description: "Quantum computing is poised to become one of the most potentially disruptive technologies of the next 30 years.")
+Trend.create(title: "Social technologies", description: "At the heart of social networking is the seed of a powerful idea - individuals can create new personas online and form virtual collectives that generate influence and action in the real world.")
+
+
+
+
